bce773c26acd56dee1c66ae6da60ff3d2641be2c
albertalvin8080 <albertalvin99@gmail.com>
2025-01-26 16:20:28 -0300

    feat: linear layout

---
+++ X02_LinearLayout_Toast/app/src/main/java/org/albert/x02_linearlayout_toast/MainActivity.kt
@@ -0,0 +1,32 @@
+package org.albert.x02_linearlayout_toast
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.albert.x02_linearlayout_toast.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnToast.setOnClickListener {
+            val text = binding.text.text.trim()
+            Toast.makeText(applicationContext, "You said: $text", Toast.LENGTH_LONG).show()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
