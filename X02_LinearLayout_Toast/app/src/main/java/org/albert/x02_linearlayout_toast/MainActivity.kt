673fc7fc60c6831403d7c2fd8c4064e8f6b03374
albertalvin8080 <albertalvin99@gmail.com>
2025-01-26 18:53:57 -0300

    feat: yen conversor

--- X02_LinearLayout_Toast/app/src/main/java/org/albert/x02_linearlayout_toast/MainActivity.kt
+++ X02_LinearLayout_Toast/app/src/main/java/org/albert/x02_linearlayout_toast/MainActivity.kt
@@ -1,6 +1,7 @@
 package org.albert.x02_linearlayout_toast
 
 import android.os.Bundle
+import android.text.Editable
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -18,9 +19,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnToast.setOnClickListener {
-            val text = binding.text.text.trim()
-            Toast.makeText(applicationContext, "You said: $text", Toast.LENGTH_LONG).show()
+        binding.btnReal.setOnClickListener {
+            fromYen(binding.yenInput.text, "BRL", 0.038808)
+        }
+
+        binding.btnDolar.setOnClickListener {
+            fromYen(binding.yenInput.text, "USD", 0.0065)
+        }
+
+        binding.btnEuro.setOnClickListener {
+            fromYen(binding.yenInput.text, "EUR", 0.0061)
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -28,5 +36,27 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun fromYen(text: Editable, currency: String, rate: Double) {
+        val text = text.toString().trim()
+        if (text.isEmpty()) {
+            binding.output.text = "Invalid number"
+            Toast.makeText(applicationContext, "Invalid number", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val yen = convert(text.toDouble(), rate)
+        show(currency, yen)
+    }
+
+    private fun convert(value: Double, rate: Double): Double {
+        return value * rate
+    }
+
+    private fun show(currency: String, value: Double) {
+        val msg = String.format("$currency: %.2f", value)
+        binding.output.text = msg
+        Toast.makeText(applicationContext, msg, Toast.LENGTH_SHORT).show()
     }
 }
