16ee3a6a578823bca9ab6c0fbcc0cb5899f6ba04
Albert Alvin Bandeira Sobral <a.bandeira.sobral@avanade.com>
2025-03-09 20:19:59 -0300

    wip: globochat

--- X09_GloboChat/app/src/main/java/org/albert/x09_globochat/AccountSettingsFragment.kt
+++ X09_GloboChat/app/src/main/java/org/albert/x09_globochat/AccountSettingsFragment.kt
@@ -1,28 +1,65 @@
 package org.albert.x09_globochat
 
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import org.albert.x09_globochat.databinding.FragmentAccountSettingsBinding
+import android.util.Log
+import androidx.core.content.res.ResourcesCompat
+import androidx.preference.MultiSelectListPreference
+import androidx.preference.Preference
+import androidx.preference.PreferenceCategory
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 
+class AccountSettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+//        setPreferencesFromResource(R.xml.account_settings, rootKey)
+        val ctx = requireContext()
 
-class AccountSettingsFragment : Fragment() {
+        val prefScreen =
+            preferenceManager.createPreferenceScreen(ctx)
 
-    private var _binding: FragmentAccountSettingsBinding? = null
-    private val binding get() = _binding!!
+        val privacyPrefCat = PreferenceCategory(ctx).apply {
+            title = getString(R.string.title_privacy)
+            isIconSpaceReserved = false
+        }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentAccountSettingsBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+        val publicInfo = MultiSelectListPreference(ctx).apply {
+            title = getString(R.string.title_public_info)
+            key = getString(R.string.key_public_info)
+            entries = resources.getStringArray(R.array.entries_public_info)
+            entryValues = resources.getStringArray(R.array.values_public_info)
+            isIconSpaceReserved = false
+        }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        val securityPrefCat = PreferenceCategory(ctx).apply {
+            title = getString(R.string.title_security)
+            isIconSpaceReserved = false
+        }
+
+        val logoutPref = Preference(ctx).apply {
+            key = getString(R.string.key_log_out)
+            title = getString(R.string.title_log_out)
+            isIconSpaceReserved = false
+        }
+
+        val delAccPref = Preference(ctx).apply {
+            key = getString(R.string.key_delete_account)
+            title = getString(R.string.title_delete_account)
+            icon = ResourcesCompat.getDrawable(resources, android.R.drawable.ic_menu_delete, null)
+        }
+
+        // WARNING: This order MUST be followed when adding preferences.
+        prefScreen.addPreference(privacyPrefCat)
+        prefScreen.addPreference(securityPrefCat)
+
+        privacyPrefCat.addPreference(publicInfo)
+
+        securityPrefCat.addPreference(logoutPref)
+        securityPrefCat.addPreference(delAccPref)
+
+        this.preferenceScreen = prefScreen
+
+        val pubInfo = PreferenceManager.getDefaultSharedPreferences(requireContext()).getStringSet(getString(R.string.key_public_info), null)
+        Log.d(this::class.simpleName, "$pubInfo")
     }
 }
